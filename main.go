da98cb1bac1cad3fa3f2689a58978708a66b8238
Bhuvnesh <bhuvnesh.awasthi@gmail.com>
2025-11-02 23:10:15 +0530

    trying

--- main.go
+++ main.go
@@ -1,24 +1,37 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
-	"log"
+	"log/slog"
 	"os"
 )
-
 
 func main() {
 	f, err := os.Open("messages.txt")
 	if err != nil {
-		log.Fatal("System encountered an error")
+		slog.Error(err.Error())
 	}
 
-	for  {
+	str := ""
+
+	for {
 		data := make([]byte, 8)
 		n, err := f.Read(data)
 		if err != nil {
 			break
 		}
-		fmt.Printf("read: %s\n", string(data[:n]))
+		data = data[:n]
+		i := bytes.IndexByte(data, '\n')
+		if i != -1 {
+			str += string(data[:i])
+			data = data[i+1:]
+			fmt.Printf("read: %s\n", str)
+			str = ""
+		}
+		str += string(data)
+	}
+	if len(str) != 0 {
+		fmt.Printf("read: %s\n", str)
 	}
 }
