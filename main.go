33b5d6fcf4d51cee257da0600a163a8fa657bead
Bhuvnesh <bhuvnesh.awasthi@gmail.com>
2025-11-02 23:34:58 +0530

    ch1

--- main.go
+++ main.go
@@ -3,35 +3,48 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"log/slog"
+	"io"
+	"log"
 	"os"
 )
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string, 1)
+
+	go func() {
+		defer f.Close()
+		defer close(ch)
+		str := ""
+		for {
+			data := make([]byte, 8)
+			n, err := f.Read(data)
+			if err != nil {
+				break
+			}
+			data = data[:n]
+			i := bytes.IndexByte(data, '\n')
+			if i != -1 {
+				str += string(data[:i])
+				data = data[i+1:]
+				ch <- str
+				str = ""
+			}
+			str += string(data)
+		}
+		if len(str) != 0 {
+			ch <- str
+		}
+	}()
+	return ch
+}
 
 func main() {
 	f, err := os.Open("messages.txt")
 	if err != nil {
-		slog.Error(err.Error())
+		log.Fatal("error", "error", err)
 	}
-
-	str := ""
-
-	for {
-		data := make([]byte, 8)
-		n, err := f.Read(data)
-		if err != nil {
-			break
-		}
-		data = data[:n]
-		i := bytes.IndexByte(data, '\n')
-		if i != -1 {
-			str += string(data[:i])
-			data = data[i+1:]
-			fmt.Printf("read: %s\n", str)
-			str = ""
-		}
-		str += string(data)
-	}
-	if len(str) != 0 {
-		fmt.Printf("read: %s\n", str)
+	output := getLinesChannel(f)
+	for val := range output {
+		fmt.Printf("read: %s\n", val)
 	}
 }
